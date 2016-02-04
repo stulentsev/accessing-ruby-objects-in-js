c223450ad700644e7c2397dee931174ac42d87c8
Sergio Tulentsev <sergei.tulentsev@gmail.com>
2016-02-04 13:38:19 +0300

    Simplify code by excluding unnecessary json parsing

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def obj_as_js_var(var_name, obj, content_for_name = nil)
-    str = "var #{var_name} = JSON.parse(\"#{obj.to_json.gsub('"', '\"')}\")"
+    str = "var #{var_name} = #{obj.to_json}"
     output = javascript_tag(str, type: 'text/javascript')
     if content_for_name
       content_for content_for_name, output
